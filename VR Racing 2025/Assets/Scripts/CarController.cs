b27a4549d4e68e123fe9d69ef366e4bcd6dfac5f
questez <ivanaristarkhov1234@gmail.com>
2025-09-13 14:12:21 +0300

    Добавление базового управление машиной
    Создан скрипт CarController. Машина может ехать и поворачивать.

--- VR Racing 2025/Assets/Scripts/CarController.cs
+++ VR Racing 2025/Assets/Scripts/CarController.cs
@@ -1,9 +1,19 @@
 using LogitechG29.Sample.Input;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using static CarControllerSample;
+
 
 public class CarController : MonoBehaviour
 {
     [SerializeField] InputControllerReader inputControllerReader;
+
+    [SerializeField] List<AxleInfo> axleInfos; // информация о каждой отдельной оси автомобиля
+
+    [SerializeField] float maxEnginePower; // максимальный крутящий момент, который двигатель может приложить к колесу (max мощность двигателя)
+
+    [SerializeField] float maxSteeringAngle; // максимальный угол поворота, который может иметь колесо
 
     private void Awake()
     {
@@ -23,10 +33,46 @@ public class CarController : MonoBehaviour
         inputControllerReader.OnOptionsCallback -= HandleOnOptionsCallback;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        //inputControllerReader.SetDebugMode(true);
+        float speed = 0f;
+
+        if (inputControllerReader.Throttle != 0)
+        {
+            speed = inputControllerReader.Throttle;
+        }
+        else if (inputControllerReader.Brake != 0)
+        {
+            speed = -inputControllerReader.Brake;
+        }
+        
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            speed = 0;
+        }
+
+        //float current_power = speed * maxEnginePower;
+        float current_power = Input.GetAxis("Vertical") * maxEnginePower;
+        //float steering_angle = maxSteeringAngle * inputControllerReader.Steering;
+        float steering_angle = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (var info in axleInfos)
+        {
+            if (info.isSteering)
+            {
+                info.rightWheel.steerAngle = steering_angle;
+                info.leftWheel.steerAngle = steering_angle;
+            }
+            if (info.isMotor)
+            {
+                info.rightWheel.motorTorque = current_power;
+                info.leftWheel.motorTorque = current_power;
+            }
+        }
     }
+
+
+
 
     private void HandleOnHomeCallback(bool value)
     {
@@ -52,7 +98,13 @@ public class CarController : MonoBehaviour
         }
     }
 
-    
 
-
+    [Serializable]
+    public class AxleInfo
+    {
+        public WheelCollider leftWheel;
+        public WheelCollider rightWheel;
+        public bool isMotor; // это колесо прикреплено к мотору?
+        public bool isSteering; // может ли это колесо поворачивать?
+    }
 }
